1b740c797554e3228ec58ad43bf8a66f24bdbab0
Lasse S. Haslev <lasse@haslev.no>
2016-07-11 16:38:26 +0200

    Added description for vue-component

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,18 @@
 <template>
-  <div id="app">
-    <h1>{{ msg }}</h1>
-    <color-picker></color-picker>
-  </div>
+    <div id="app">
+        <h1>Vue input fields</h1>
+        <p>This package contains input fields for vuejs</p>
+        <p>It should be used the same way as nor mal input fields</p>
+        <p>Main concept is you give a :model="" with the data the same way you would use v-model="" on normal input element</p>
+
+        <h3>Color picker</h3>
+        <color-picker :model="'#005599'"></color-picker>
+    </div>
 </template>
 
 <script>
 import ColorPicker from './Inputs/Color/ColorPicker.vue';
 export default {
-    data () {
-        return {
-        // note: changing this line won't causes changes
-        // with hot-reload because the reloaded component
-        // preserves its current state and we are modifying
-        // its initial state.
-        msg: 'Hello Vue!'
-        }
-    },
 
     components: {
 
